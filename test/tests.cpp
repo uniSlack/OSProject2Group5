731d414c415ce616a0d0fbdbb5e377c351ad14d9
Jacob Howard <jacobhoward@ksu.edu>
2024-02-17 13:05:41 -0600

    Formating fix in test.cpp from anouncement

--- test/tests.cpp
+++ test/tests.cpp
@@ -8,32 +8,7 @@
 extern "C" 
 {
 #include <dyn_array.h>
-
 }
-
-
-#define NUM_PCB 30
-#define QUANTUM 5 // Used for Robin Round for process as the run time limit
-
-unsigned int score;
-unsigned int total;
-
-class GradeEnvironment : public testing::Environment 
-{
-    public:
-        virtual void SetUp() 
-        {
-            score = 0;
-            total = 210;
-        }
-
-        virtual void TearDown()
-        {
-            ::testing::Test::RecordProperty("points_given", score);
-            ::testing::Test::RecordProperty("points_total", total);
-            std::cout << "SCORE: " << score << '/' << total << std::endl;
-        }
-};
 
 TEST (load_process_control_blocks, NullInputFilename) {
     const char *input_filename = NULL;
@@ -69,9 +44,35 @@ TEST (load_process_control_blocks, GoodRead) {
     EXPECT_FALSE(res == NULL);
 }
 
-int main(int argc, char **argv) 
+int main() 
 {
-    ::testing::InitGoogleTest(&argc, argv);
-    ::testing::AddGlobalTestEnvironment(new GradeEnvironment);
+    //::testing::InitGoogleTest(&argc, argv);
+    //::testing::AddGlobalTestEnvironment(new GradeEnvironment);
     return RUN_ALL_TESTS();
 }
+
+/*
+#define NUM_PCB 30
+#define QUANTUM 5 // Used for Robin Round for process as the run time limit
+
+unsigned int score;
+unsigned int total;
+
+class GradeEnvironment : public testing::Environment 
+{
+    public:
+        virtual void SetUp() 
+        {
+            score = 0;
+            total = 210;
+        }
+
+        virtual void TearDown()
+        {
+            ::testing::Test::RecordProperty("points_given", score);
+            ::testing::Test::RecordProperty("points_total", total);
+            std::cout << "SCORE: " << score << '/' << total << std::endl;
+        }
+};
+*/
+
